55ca9e79e0af657fd2e5d70192fa8e6bfd25ab43
Sabbir-97 <mdsabbirahmmed87@gmail.com>
2021-10-31 10:15:19 +0600

    second commit

---
+++ olms-app/resources/views/come.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/../public/sabbir.css">
+</head>
+<body>
+    <section class="navbar">
+        <div class="container">
+            <div class="logo">
+                <h1>logo</h1>
+            </div>
+            <div class="logo_title">
+                <h1>title</h1>
+            </div>
+            <div class="menu">
+                <h1>menu</h1>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
---
+++ olms2-app/resources/views/sabbir.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1>
+            My first post
+
+        
+        </h1>
+        <p>
+        Here I have written 100 Computer Science Projects with the complete description and database design. Database design is fully not completed. But you can use it. If you are looking for any Project Help Services. You will get awesome quality custom projects according to your requirements.
+        </p>
+    </article>
+</body>
